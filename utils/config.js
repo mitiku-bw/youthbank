74036dead7d9f0ae06827c93e74999bc0904de31
Mitiku Wubtie <mitikubw@gmail.com>
2019-09-23 01:47:44 +0300

    Fixing no application error

--- utils/config.js
+++ utils/config.js
@@ -1,7 +1,7 @@
 // require('dotenv').config()
 
 let PORT = process.env.PORT || 3003
-let MONGODB_URI = MONGODB_URI=`mongodb+srv://mite:phonebook@cluster0-xob8q.mongodb.net/test?retryWrites=true`
+let MONGODB_URI =`mongodb+srv://mite:phonebook@cluster0-xob8q.mongodb.net/test?retryWrites=true`
 
 // let MONGODB_URI = process.env.MONGODB_URI
 
