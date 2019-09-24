a628fea4410310ceb81cbcda66d00d6082f704c1
Mitiku Wubtie <mitikubw@gmail.com>
2019-09-25 00:59:29 +0300

    Minor typo on app.js

--- app.js
+++ app.js
@@ -26,9 +26,9 @@ app.use(cors())
 app.use(bodyParser.json())
 app.use(middleware.requestLogger)
 if(process.env.NODE_ENV === 'production'){
-  app.use(app.use(express.static('build')))
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'build', 'index.html'))
+  app.use(express.static(path.join(__dirname, 'build')))
+  app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
   })
 }
 
